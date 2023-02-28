e67c362e1047e5c517a5ee9a414bb77fae47d09f
SpacetimeCadet <56453761+SpacetimeCadet@users.noreply.github.com>
2023-02-27 19:49:20 -0500

    link test complete

--- network.py
+++ network.py
@@ -1,8 +1,21 @@
 class Network():
-    def __init__(self, nodes, links, weights):
+    def __init__(self, nodes, links):
         self.nodes = nodes
         self.links = links
-        self.weights = weights
 
     def getNodeList(self):
         return self.nodes
+    
+    def getLinkList(self):
+        return self.links
+    
+    def getLinksOf(self, node):
+        l = []
+        for i in self.links:
+            if i[0] == node or i[1] == node:
+                l.append(i)
+        return l
+
+        
+    
+    
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from . import network
+from network import Network
 
-n = Network([1, 2, 3, 4], [[1, 2], [2, 3], [2, 4], [3, 4]], [5, 10, 15, 5])
-print(n.getNodeList())
+n = Network([1, 2, 3, 4], [[1, 2, 5], [2, 3, 10], [2, 4, 15], [3, 4, 5]])
+
+print(n.getLinksOf(2))
