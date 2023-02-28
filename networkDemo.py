c9bd5a209ea1382c8a340a17b6a81b015a371532
SpacetimeCadet <56453761+SpacetimeCadet@users.noreply.github.com>
2023-02-27 22:13:57 -0500

    networkDemo updated

--- networkDemo.py
+++ networkDemo.py
@@ -2,6 +2,7 @@ from network import Network
 
 n = Network([1, 2, 3, 4], [[1, 2, 5], [2, 3, 10], [2, 4, 15], [3, 4, 5]])
 
+# todo: complete all methods
 print("- - - - -")
 print(n.nodes)
 print(n.links)
