0d4e41ebc103d3850fbe373ac28c2b63db737694
SpacetimeCadet <56453761+SpacetimeCadet@users.noreply.github.com>
2023-02-27 22:17:01 -0500

    networkDemo updated

--- networkDemo.py
+++ networkDemo.py
@@ -3,6 +3,7 @@ from network import Network
 # generateNetwork() is still under development
 
 # ex. instantiate manually
+# n = Network([nodes], [links]); where links are [source, destination, weight]
 n = Network([1, 2, 3, 4], [[1, 2, 5], [2, 3, 10], [2, 4, 15], [3, 4, 5]])
 
 print("- - - - -")
