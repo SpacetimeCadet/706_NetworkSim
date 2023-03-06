793e6b6913d5230a664e81caf84cf004318a2f4f
Maeve Fitzgerald <maevefitzgerald@hotmail.ca>
2023-03-06 21:35:08 +0000

    Buttons adjust to screen size

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from pygame.locals import QUIT
 pygame.init()
 
 #Screen setup
-height = 1120
-width = 2025
+#original size: Height = 1120, Width = 2025
+height = 800
+width = 1600
 size = [width, height]
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption('Connection Model')
 
 objects = []
 
 #style: 1 = teal theme, network background, 2 = blue theme, spiral background
-theme.style = 2
+theme.style = 1
 currentStyle = theme.currentTheme()
 
 class Data():
@@ -54,11 +55,13 @@ def toggleState():
 def setupState():
     #get user input on routers, state=True
     if not data.stateSetupDone:
+        fontSize = width*0.013
         objects.extend([
-            Button(75, 200, 250, 60, 'ADD ROUTER', addRouter),
-            Button(375, 200, 250, 60, 'ADD CONNECTION', addConnection),
+            Button(width*0.04, height*0.18, width*0.12, height*0.05, fontSize, 'ADD ROUTER', addRouter),
+            Button(width*0.19, height*0.18, width*0.13, height*0.05, fontSize, 'ADD CONNECTION', addConnection),
             #probably remove "add connection" button, trigged by draging from one router to another
-            Button(1000, 900, 400, 60, 'USE THIS DATA', toggleState)
+            Button(width*0.34, height*0.18, width*0.13, height*0.05, fontSize, 'RANDOMIZE DATA', addConnection),
+            Button(width*0.4, height*0.8, width*0.2, height*0.05, fontSize, 'USE THIS DATA', toggleState)
             ])
         data.stateSetupDone = True
 
@@ -66,19 +69,20 @@ def setupState():
 def traceState():
     #main state, algoritms performed on router data, state=False
     if not data.stateSetupDone:
+        fontSize = width*0.013
         objects.extend([
-            Button(75, 200, 250, 60, 'SENDING PORT', selectSendPort),
-            Button(375, 200, 250, 60, 'ALGORITHM', selectAlgorithm),
-            Button(675, 200, 250, 60, 'RECIEVING PORT', selectRecievePort),
-            Button(1000, 900, 400, 60, 'CHANGE ROUTER DATA', toggleState)
+            Button(width*0.04, height*0.18, width*0.12, height*0.05, fontSize, 'SENDING PORT', selectSendPort),
+            Button(width*0.19, height*0.18, width*0.12, height*0.05, fontSize, 'ALGORITHM', selectAlgorithm),
+            Button(width*0.34, height*0.18, width*0.12, height*0.05, fontSize, 'RECIEVING PORT', selectRecievePort),
+            Button(width*0.4, height*0.8, width*0.2, height*0.05, fontSize, 'CHANGE ROUTER DATA', toggleState)
             ])
         data.stateSetupDone = True
 
 
 def draw(size):
     screen.blit(currentStyle.get("background"), (0, 0))
-    centreText("CPS706 PROJECT", theme.medFont, 100, currentStyle.get("titleColour"), 100)
-    text("BY [GROUP MEMBERS]", theme.medFont, 25, currentStyle.get("titleColour"), 575, 150)
+    centreText("CPS706 PROJECT", theme.medFont, int(width*0.05), currentStyle.get("titleColour"), height*0.1)
+    text("BY [GROUP MEMBERS]", theme.medFont, int(0.01*width), currentStyle.get("titleColour"), width*0.3, height*0.14)
     if data.state:
         setupState()
     else:
@@ -110,6 +114,15 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.VIDEORESIZE:
+            # There's some code to add back window content here.
+            screen = pygame.display.set_mode((event.w, event.h),
+                                              pygame.RESIZABLE)
     draw(size)
     pygame.display.update()
     #fpsClock.tick(fps)
+
+
+
+
+        
