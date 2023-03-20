d96f5f1d682b6e3aee0ec69b1b1117fdbc59ade3
RRodin400 <126524183+RRodin400@users.noreply.github.com>
2023-03-20 17:34:50 -0400

    Add files via upload

---
+++ routerNode.py
@@ -0,0 +1,49 @@
+#this file defines the routerNode class
+
+class routerNode:
+    def __init__(self, name):
+        """
+        () -> object with set attributes
+        
+        This function is a constructor for the class. We use built in function
+        "__init__" to set the attributes for objects of the class routerNode.
+        
+        So the class routerNode has the attributes cost and pred and it is 
+        setting them with default values infinity for the cost and an empty list
+        for pred.
+        
+        """
+        self.name = name
+        self.cost = float('inf')
+        self.pred = []
+        
+    #how the object will print:
+    def __str__(self):
+        return f"{self.name}: cost: {self.cost}, pred: {self.pred}" 
+
+        
+    def updateCost(self, givenCost):
+        """
+        Sets the cost attribute for an object of the routerNode class. 
+        """
+        self.cost = givenCost
+        
+    def updatePred(self, givenPred):
+        """
+        Sets the cost attribute for an object of the routerNode class. 
+        """
+        self.pred = givenPred
+        
+    def getCost (self):
+        """
+        Return the value of the cost attribute of the object
+        """
+        return self.cost
+    
+    def getPred(self):
+        """
+        Return the value of the predessor attribute of the object
+        """
+        return self.pred
+       
+   
